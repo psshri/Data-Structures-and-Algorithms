8b5bfd6d725f2094a2c9bc5c0cb0f93e0a8f2fb7
Priyanshu <pshekhar830@gmail.com>
2023-04-17 12:21:36 +0530

    completed c++ stl

--- theory/C++ STL/main.cpp
+++ theory/C++ STL/main.cpp
@@ -1,3 +1,7 @@
+//#include<bits/stdc++.h>
+#include<algorithm>
+#include<map>
+#include<set>
 #include<queue>
 #include<stack>
 #include<list>
@@ -248,7 +252,115 @@ int main(){
 
     cout<<"Priority queue empty? "<<maxi.empty()<<endl;
 
+    // Set ////////////////////////////////////////////////////////////////////////////////////////
+    cout<<endl<<"Set *********************************************************************************"<<endl<<endl;    
 
+    set<int> set1;
+    set1.insert(4);
+    set1.insert(3);
+    set1.insert(1);
+    set1.insert(4);
+
+    for(auto i:set1){
+        cout<<i<<endl;
+    }
+    cout<<endl;
+
+    set<int>::iterator it_set = set1.begin();
+    it_set++;
+
+    set1.erase(it_set);
+
+    for(auto i:set1){
+        cout<<i<<endl;
+    }
+
+    // tells whether an element is present or not
+    cout<<"4 is present or not? "<<set1.count(4)<<endl;
+
+    // find gives you the iterator to an element
+    set<int>::iterator it_set2 = set1.find(4);
+    cout<<"value present at it_set2 is: "<<*it_set2<<endl;
+
+    // Map ////////////////////////////////////////////////////////////////////////////////////////
+    cout<<endl<<"Map *********************************************************************************"<<endl<<endl;
+
+    map<int, string> map1;
+
+    map1[1] = "abc";
+    map1[2] = "zxc";
+    map1.insert({3, "qwe"});
+
+    for(auto i:map1){
+        cout<<i.first<<" "<<i.second<<endl;
+    }
+
+    // count: tells whether an element is present or not
+    cout<<"Is 3 present? "<<map1.count(3)<<endl;
+    // cout<<"Is qew present? "<<map1.count("qwe")<<endl;
+
+
+    map1.erase(3);
+
+    for(auto i:map1){
+        cout<<i.first<<" "<<i.second<<endl;
+    }
+
+    // find: tells you the iterator to the element
+    map<int, string>::iterator it_map = map1.find(1);
+
+    cout<<"value at it_map: "<<it_map->first<<" "<<it_map->second<<endl;
+
+    // Algorithms ////////////////////////////////////////////////////////////////////////////////////////
+    cout<<endl<<"Algorithms *********************************************************************************"<<endl<<endl;
+
+    vector<int> v2;
+
+    v2.push_back(1);
+    v2.push_back(2);
+    v2.push_back(10);
+    v2.push_back(3);
+    v2.push_back(4);
+    v2.push_back(5);
+
+    cout<<"searching for 6 in v2: "<<binary_search(v2.begin(), v2.end(), 6)<<endl;
+    cout<<"searching for 0 in v2: "<<binary_search(v2.begin(), v2.end(), 0)<<endl;
+
+    cout<<"lower_bound of 3 in v2: "<<lower_bound(v2.begin(), v2.end(), 3)-v2.begin()<<endl;
+    cout<<"lower_bound of 5 in v2: "<<lower_bound(v2.begin(), v2.end(), 5)-v2.begin()<<endl;    
+    cout<<"upper_bound of 5 in v2: "<<upper_bound(v2.begin(), v2.end(), 5)-v2.begin()<<endl;
+
+    int a = 1;
+    int b = 2;
+
+    cout<<"min: "<<min(a, b)<<endl;
+    cout<<"max: "<<max(a, b)<<endl;
+    swap(a, b);
+    cout<<"a: "<<a<<endl;
+    cout<<"b: "<<b<<endl;
+
+    string a2 = "abcd";
+    cout<<"string: "<<a2<<endl;
+    reverse(a2.begin(), a2.end());
+    cout<<"string after reverse: "<<a2<<endl;
+
+    for(int i:v2){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    rotate(v2.begin(), v2.begin()+2, v2.end());
+    cout<<"Vector after rotate: "<<endl;
+    for(int i:v2){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+    sort(v2.begin(), v2.end());
+    cout<<"v2 after sorting: "<<endl;
+    for(int i:v2){
+        cout<<i<<" ";
+    }
+    cout<<endl;
 }
 
 // 38:21
